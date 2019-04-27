513f4fc23fd986b4318dd328d3bc4675a42d4839
HELLO MY FRIEND, AHSIH <ahsih1zhang@hotmail.com>
2019-04-27 23:43:03 +0100

    Update menu code
    Update menu code

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Menu; 
 use App\HeadDishes;
 use App\ChefSpecial;
+use App\Starters;
 
 class MenuController extends Controller {
 
@@ -13,12 +14,17 @@ class MenuController extends Controller {
         $menus = Menu::All();
         $chefSpecials = ChefSpecial::All();
         $HeadDishes = HeadDishes::All();
+        $Starters = Starters::All();
+
+        $chefSpecialSpicy = array(4,8,11,12,14,16);
 
         $data = array
         (      
          'menus' =>  $menus,   
          'headDishes' => $HeadDishes,
          'chefSpecials' => $chefSpecials,
+         'spicyFoodLabel' => $chefSpecialSpicy,
+         'starters' => $Starters,
         );
                 
         return view('menu')->with($data);
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -21,11 +21,11 @@
     <div id="Menu">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-2">
+                <div class="col-lg-3 col-sm-0 col-md-0">
                 </div>
-                <div class="col-7">
+                <div class="col-lg-7 col-sm-8 col-md-8">
                     <div class="menu-border">
-                        <div class="panel panel-chefSpecial-style">
+                        <div id="chefSpecial" class="panel panel-menu-style">
                             <div class="panel-heading">
                                 <p class="SubMenuTitle">
                                     {{ $headDishes->where('id','=','23')->first()->Dish_Name }}
@@ -33,13 +33,95 @@
                             </div>
                             <div class="panel-body">
                                 @foreach ($chefSpecials as $chefSpecial)
-                                <p>{{ $chefSpecial->FoodName }}</p>
-                                @endforeach
+                                <p class="foodNameLabel">{{ $chefSpecial->FoodName }}</p>
+                                @if (in_array($chefSpecial->id,$spicyFoodLabel))
+                                <p class="fas fa-pepper-hot pepperLabel"></p>
+                                @endif
+                                <p class="priceLabel">£{{ $chefSpecial->Price }}</p>
+                                <br class="clearFloat" />
+                                <p class="hintNameLabel">{{ $chefSpecial->HelpText }}</p>
+                                <hr class="seperateLine"> @endforeach
+                            </div>
+                        </div>
+                        <div id="Starters" class="panel panel-menu-style">
+                            <div class="panel-heading">
+                                <p class="SubMenuTitle">
+                                    {{ $headDishes->where('id','=','20')->first()->Dish_Name }}
+                                </p>
+                                <p class="SubMenuTitleHelpText">
+                                    {{ $headDishes->where('id','=','20')->first()->HelpText }}
+                                </p>
+                            </div>
+                            <div class="panel-body">
+                                @foreach ($starters as $starter)
+                                <p class="foodNameLabel">{{ $starter->FoodName }}</p>
+                                @if ($starter->Recommended)
+                                <i style='font-size:16px; color:red; margin-top:5px' class='fas'>&#xf164;</i></h6>
+                                @endif
+                                <p class="priceLabel">Small £{{ $starter->Small }}, Large £{{ $starter->Large }}</p>
+                                <br class="clearFloat" />
+                                <p class="hintNameLabel">{{ $starter->HelpText }}</p>
+                                <hr class="seperateLine"> @endforeach
+                            </div>
+                        </div>
+                        <div id="FriedRice" class="panel panel-menu-style">
+                            <div class="panel-heading">
+                                <p class="SubMenuTitle">
+                                    {{ $headDishes->where('id','=','7')->first()->Dish_Name }}
+                                </p>
+                            </div>
+                            <div class="panel-body">
+                                @foreach ($menus->where('HeadDishes_Id','=','7') as $menu)
+                                <p class="foodNameLabel">{{ $menu->FoodName }}</p>
+                                @if ($menu->Spicyness)
+                                <p class="fas fa-pepper-hot pepperLabel"></p>
+                                <i style='font-size:16px; color:red' class='fas'>&#xf164;</i></h6>
+                                @endif
+                                <p class="priceLabel">£{{ $menu->Price }}</p>
+                                <br class="clearFloat" />
+                                <p class="hintNameLabel">{{ $menu->HelpText }}</p>
+                                <hr class="seperateLine"> @endforeach
+                            </div>
+                        </div>
+                        <div id="Chowmein" class="panel panel-menu-style">
+                            <div class="panel-heading">
+                                <p class="SubMenuTitle">
+                                    {{ $headDishes->where('id','=','22')->first()->Dish_Name }}
+                                </p>
+                            </div>
+                            <div class="panel-body">
+                                @foreach ($menus->where('HeadDishes_Id','=','22') as $menu)
+                                <p class="foodNameLabel">{{ $menu->FoodName }}</p>
+                                @if ($menu->Spicyness)
+                                <p class="fas fa-pepper-hot pepperLabel"></p>
+                                @endif
+                                <p class="priceLabel">£{{ $menu->Price }}</p>
+                                <br class="clearFloat" />
+                                <p class="hintNameLabel">{{ $menu->HelpText }}</p>
+                                <hr class="seperateLine"> @endforeach
+                            </div>
+                        </div>
+                        <div id="SetMeal" class="panel panel-menu-style">
+                            <div class="panel-heading">
+                                <p class="SubMenuTitle">
+                                    {{ $headDishes->where('id','=','25')->first()->Dish_Name }}
+                                </p>
+                            </div>
+                            <div class="panel-body">
+                                @foreach ($menus->where('HeadDishes_Id','=','25') as $menu)
+                                <p class="foodNameLabel">{{ $menu->FoodName }}</p>
+                                @if ($menu->Spicyness)
+                                <p class="fas fa-pepper-hot pepperLabel"></p>
+                                @endif
+                                <p class="priceLabel">£{{ $menu->Price }}</p>
+                                <br class="clearFloat" />
+                                <p class="hintNameLabel">{{ $menu->HelpText }}</p>
+                                <hr class="seperateLine"> @endforeach
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-3">
+                <div class="col-lg-2 col-sm-4 col-md-4">
 
                 </div>
             </div>
