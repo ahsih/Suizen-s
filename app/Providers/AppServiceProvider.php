17c932e07af033cd27230b051c0ed53f9c8aee32
HELLO MY FRIEND, AHSIH <ahsih1zhang@hotmail.com>
2019-08-13 22:48:42 +0100

    Update AppServiceRPovider
    Update AppServiceRPovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if($this->app->environment('production'))
+        {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
