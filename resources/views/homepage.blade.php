bf67de785981f310a88889282d82f42f31ac68ec
HELLO MY FRIEND, AHSIH <ahsih1zhang@hotmail.com>
2019-09-08 17:43:28 +0100

    Update homepage
    Update homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="Suizen's Noodle Bar 广东楼伯明翰大学" />
+    <meta name="google-site-verification" content="_V-arpPAUgr_mMGBkU85VklBJ1oG7qoNji1FI12DLaY" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- CSS -->
