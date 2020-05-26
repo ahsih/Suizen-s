747fa89a4195d166df9420c690ba95fc493b6791
HELLO MY FRIEND, AHSIH <ahsih1zhang@hotmail.com>
2020-05-26 12:59:49 +0100

    Delete notice message
    Delete notice message

--- resources/views/include/Notice.blade.php
+++ resources/views/include/Notice.blade.php
@@ -7,5 +7,4 @@
 ?>
 <div class="container-fluid noticeBackgroundColor">
     <p class="NoticeText">We are reopening from Sat 30th May Every-day From 12pm to 3pm and 4pm to 9pm</p>
-    <p class="NoticeText">Until Further notice</p>
 </div>
