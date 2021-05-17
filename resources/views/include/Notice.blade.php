f9a046ba2a3491d72baa3c4f7e53483854bca63a
HELLO MY FRIEND, AHSIH <ahsih1zhang@hotmail.com>
2021-05-17 20:11:47 +0100

    push changes
    push changes

--- resources/views/include/Notice.blade.php
+++ resources/views/include/Notice.blade.php
@@ -5,7 +5,8 @@
  * Time: 18:35
  */
 ?>
-<!-- div class="container-fluid noticeBackgroundColor">
-    <p class="NoticeText">Due to the national lockdown, our business is open for Monday till Saturday 5pm to 8:30pm</p>
+<div class="container-fluid noticeBackgroundColor">
+</div>
+    <!--p class="NoticeText">Due to the national lockdown, our business is open for Monday till Saturday 5pm to 8:30pm</p>
     <p class="NoticeText">Check out our Google 'Suizen Noodle Bar' for our latest opening time, we will be updating weekly</p>
 </div>
