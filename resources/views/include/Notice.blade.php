cb445e57fdacde7de13aab20ee5cb2bd3016c838
HELLO MY FRIEND, AHSIH <ahsih1zhang@hotmail.com>
2022-02-07 00:43:07 +0000

    Update notice hours
    Update notice hours

--- resources/views/include/Notice.blade.php
+++ resources/views/include/Notice.blade.php
@@ -3,8 +3,9 @@
  * User: Jack
  * Date: 01/04/2020
  * Time: 18:35
+ * Open Monday to Saturday 11:30pm to 10pm
  */
 ?>
 <div class="container-fluid noticeBackgroundColor">
-    <p class="NoticeText">Open Monday to Saturday 11:30pm to 10pm </p>
+    <p class="NoticeText"> We will be closed From Friday 11st Feb to Sunday 13th Feb, sorry for any inconvenience </p>
 </div>
