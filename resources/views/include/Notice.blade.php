0b2573b39596f54cff7fb0c1b79cafd56639bbbf
HELLO MY FRIEND, AHSIH <ahsih1zhang@hotmail.com>
2022-09-09 16:18:42 +0100

    Updated notice
    Updated notice

--- resources/views/include/Notice.blade.php
+++ resources/views/include/Notice.blade.php
@@ -7,6 +7,6 @@
  */
 ?>
 <div class="container-fluid noticeBackgroundColor">
-    <p class="NoticeText">Price increase due to energy price spiked</p>
-    <p class="NoticeText"> Temporary Accept Cash Only :)</p>
+    <p class="NoticeText">We will closed from Sunday 11/09/22 till Friday 16/09/22 - For Maintenance </p>
+    <p class="noticeText">Sorry for any inconvenience</p>
 </div>
