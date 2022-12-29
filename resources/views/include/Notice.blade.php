04e66ac6a16a7ee548ebb0eccd0ece9c99450731
HELLO MY FRIEND, AHSIH <ahsih1zhang@hotmail.com>
2022-12-29 11:30:27 +0000

    Updated menu and notice time
    Updated menu and notice time

--- resources/views/include/Notice.blade.php
+++ resources/views/include/Notice.blade.php
@@ -7,6 +7,6 @@
  */
 ?>
 <div class="container-fluid noticeBackgroundColor">
-    <p class="NoticeText">We will closed from 24th Dec to 28th Dec</p>
+    <p class="NoticeText">We will closed from 24th Dec to 29th Dec</p>
     <p class="NoticeText">Merry Christmas and Happy New Year</p>
 </div>
