f2833437296acf1e663a0b66aa2d013e83708682
HELLO MY FRIEND, AHSIH <ahsih1zhang@hotmail.com>
2023-08-24 23:09:50 +0100

    Updated pricing
    Updated pricing

--- resources/views/include/Notice.blade.php
+++ resources/views/include/Notice.blade.php
@@ -6,7 +6,8 @@
  * Open Monday to Saturday 11:30pm to 10pm
  */
 ?>
+<!--
 <div class="container-fluid noticeBackgroundColor">
     <p class="NoticeText">Price Change in September due to Energy contract finish in Sep 2023</p>
     <p class="NoticeText">>We are very sorry for any inconvenience</p>
-</div>
+</div>-->
