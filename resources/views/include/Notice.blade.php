059b3339af508c6b54861c4a474addd3e4fe2f0c
Ahsih <ahsih1zhang@hotmail.com>
2024-04-15 10:54:38 +0100

    Updated notice
    Updated notice

--- resources/views/include/Notice.blade.php
+++ resources/views/include/Notice.blade.php
@@ -6,7 +6,8 @@
  * Open Monday to Saturday 11:30pm to 10pm
  */
 ?>
+<!--
 <div class="container-fluid noticeBackgroundColor">
     <p class="NoticeText">We will closed from 23rd March 2024 to 13th April</p>
     <p class="NoticeText">>We are very sorry for any inconvenience</p>
-</div>
+</div>-->
