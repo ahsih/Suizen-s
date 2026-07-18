ffa599c1710be918e5c0334c2b8af4d4de016b74
Ahsih <ahsih1zhang@hotmail.com>
2026-07-18 12:41:30 +0100

    Update Notice.blade.php

--- resources/views/include/Notice.blade.php
+++ resources/views/include/Notice.blade.php
@@ -7,5 +7,6 @@
  */
 ?>
 <div class="container-fluid noticeBackgroundColor NoticeText">
+    We will be closed from 19th July to 30th July· 我们将于7月19日至7月30日放年假.
     Cash only at the moment · 目前只收现金.
 </div>
